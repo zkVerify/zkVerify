aac64a001515d23e390c4e708ab3ca6e0bf37b54
DimitrisPapac <jim.papachristoudis@gmail.com>
2024-12-23 18:04:02 +0100

    Update native/src/accelerated_bn/bn254.rs
    Co-authored-by: Michele d'Amico <michele.damico@gmail.com>

--- native/src/accelerated_bn/bn254.rs
+++ native/src/accelerated_bn/bn254.rs
@@ -122,7 +122,8 @@ impl CurveHooks for HostHooks {
         let scalar = utils::encode(scalar);
         let res = host_calls::bn254_mul_projective_g2(base, scalar).unwrap_or_default();
         utils::decode_proj_sw(res).map_err(|_| ())
-    }
+        let res = host_calls::bn254_mul_projective_g2(base.as_slice(), scalar.as_slice()).unwrap_or_default();
+        utils::decode_proj_sw(res.as_slice()).map_err(|_| ())
 }
 
 /// Interfaces for working with *Arkworks* *BN254* elliptic curve related types
