37f3ff8d039772bb47ee296d8aa6bb103331303d
Michele d'Amico <michele.damico@gmail.com>
2025-02-13 15:02:12 +0100

    For benchmarking generate genesis from wasm instead of chain spec (#226)

--- scripts/bench_cfg.sh
+++ scripts/bench_cfg.sh
@@ -6,6 +6,7 @@ DEFAULT_BM_HEAP_PAGES=4096
 PROJECT_ROOT=${PROJECT_ROOT:-$(git rev-parse --show-toplevel)}
 WEIGTH_OUT_PATH=${WEIGTH_OUT_PATH:-""}
 ZKV_NODE_EXE=${ZKV_NODE_EXE:-"${PROJECT_ROOT}/target/production/zkv-node"}
+ZKV_RUNTIME=${ZKV_RUNTIME:-"${PROJECT_ROOT}/target/production/wbuild/zkv-runtime/zkv_runtime.compact.compressed.wasm"}
 BM_STEPS=${BM_STEPS:-${DEFAULT_BM_STEPS}}
 BM_REPEAT=${BM_REPEAT:-${DEFAULT_BM_REPEAT}}
 BM_HEAP_PAGES=${BM_HEAP_PAGES:-${DEFAULT_BM_HEAP_PAGES}}
