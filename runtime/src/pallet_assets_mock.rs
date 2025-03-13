4f45ceb8962a50f60b6b3d58fa776569e0f15507
Rodrigo Doria Medina <rodrigo@horizenlabs.io>
2025-03-13 12:30:51 -0600

    feat: address comments

--- runtime/src/pallet_assets_mock.rs
+++ runtime/src/pallet_assets_mock.rs
@@ -8,7 +8,8 @@ use frame_support::{
     pallet_prelude::*,
     traits::{
         tokens::{
-            fungibles, DepositConsequence, Fortitude, Precision, Preservation, Provenance,
+            fungibles::{self, Dust},
+            DepositConsequence, Fortitude, Precision, Preservation, Provenance,
             WithdrawConsequence,
         },
         Currency,
@@ -16,7 +17,7 @@ use frame_support::{
 };
 use scale_info::TypeInfo;
 use sp_runtime::{
-    traits::{AtLeast32BitUnsigned, StaticLookup, Zero},
+    traits::{AtLeast32BitUnsigned, Zero},
     DispatchError,
 };
 use sp_std::{marker::PhantomData, prelude::*};
@@ -29,7 +30,7 @@ pub trait Config: frame_system::Config {
     /// The asset ID type
     type AssetId: Member + Parameter + MaxEncodedLen + Copy + TypeInfo + Debug;
 
-    /// The balance type - note all the required traits
+    /// The balance type
     type Balance: Member
         + Parameter
         + MaxEncodedLen
@@ -47,29 +48,25 @@ pub trait Config: frame_system::Config {
 // Mock implementation of Assets pallet
 pub struct Pallet<T>(PhantomData<T>);
 
-// MOCK: Implementation of fungibles::Inspect for Assets
+// MOCK: Implementation of fungibles::Inspect for Assets (required by Mutate)
 impl<T: Config> fungibles::Inspect<T::AccountId> for Pallet<T> {
     type AssetId = T::AssetId;
     type Balance = T::Balance;
 
-    fn asset_exists(_id: Self::AssetId) -> bool {
-        true // Mock always returns true
-    }
-
     fn total_issuance(_id: Self::AssetId) -> Self::Balance {
-        T::Balance::from(1_000_000u128) // Mock constant value
+        T::Balance::from(1_000_000u128)
     }
 
     fn minimum_balance(_id: Self::AssetId) -> Self::Balance {
-        T::Balance::from(1u128) // Mock constant value
+        T::Balance::from(1u128)
     }
 
     fn balance(_id: Self::AssetId, _who: &T::AccountId) -> Self::Balance {
-        T::Balance::from(1_000u128) // Mock constant value
+        T::Balance::from(1_000u128)
     }
 
     fn total_balance(_id: Self::AssetId, _who: &T::AccountId) -> Self::Balance {
-        T::Balance::from(1_000u128) // Mock constant value
+        T::Balance::from(1_000u128)
     }
 
     fn reducible_balance(
@@ -78,7 +75,7 @@ impl<T: Config> fungibles::Inspect<T::AccountId> for Pallet<T> {
         _keep_alive: Preservation,
         _force: Fortitude,
     ) -> Self::Balance {
-        T::Balance::from(1_000u128) // Mock constant value
+        T::Balance::from(1_000u128)
     }
 
     fn can_deposit(
@@ -87,7 +84,7 @@ impl<T: Config> fungibles::Inspect<T::AccountId> for Pallet<T> {
         _amount: Self::Balance,
         _mint: Provenance,
     ) -> DepositConsequence {
-        DepositConsequence::Success // Always succeed
+        DepositConsequence::Success
     }
 
     fn can_withdraw(
@@ -95,29 +92,11 @@ impl<T: Config> fungibles::Inspect<T::AccountId> for Pallet<T> {
         _who: &T::AccountId,
         _amount: Self::Balance,
     ) -> WithdrawConsequence<Self::Balance> {
-        WithdrawConsequence::Success // Always succeed
-    }
-}
-
-// MOCK: Implementation of fungibles::Mutate for Assets
-impl<T: Config> fungibles::Mutate<T::AccountId> for Pallet<T> {
-    fn mint_into(
-        _id: Self::AssetId,
-        _who: &T::AccountId,
-        _amount: Self::Balance,
-    ) -> Result<Self::Balance, DispatchError> {
-        Ok(T::Balance::from(0u128)) // Just return success with zero
+        WithdrawConsequence::Success
     }
 
-    fn burn_from(
-        _id: Self::AssetId,
-        _who: &T::AccountId,
-        _amount: Self::Balance,
-        _keep_alive: Preservation,
-        _precision: Precision,
-        _force: Fortitude,
-    ) -> Result<Self::Balance, DispatchError> {
-        Ok(T::Balance::from(0u128)) // Just return success with zero
+    fn asset_exists(_id: Self::AssetId) -> bool {
+        true
     }
 }
 
@@ -147,11 +126,6 @@ impl<T: Config> fungibles::Unbalanced<T::AccountId> for Pallet<T> {
         Ok(T::Balance::from(0u128))
     }
 
-    // Added missing functions from the error message
-    fn handle_dust(_dust: fungibles::Dust<T::AccountId, Self>) {
-        // no-op
-    }
-
     fn write_balance(
         _id: Self::AssetId,
         _who: &T::AccountId,
@@ -159,9 +133,35 @@ impl<T: Config> fungibles::Unbalanced<T::AccountId> for Pallet<T> {
     ) -> Result<Option<Self::Balance>, DispatchError> {
         Ok(Some(T::Balance::from(0u128)))
     }
+
+    fn handle_dust(_dust: Dust<T::AccountId, Self>) {
+        // no-op
+    }
 }
 
-// MOCK: Implementation of fungibles::metadata::Inspect for Assets
+// MOCK: Implementation of fungibles::Mutate for Assets
+impl<T: Config> fungibles::Mutate<T::AccountId> for Pallet<T> {
+    fn mint_into(
+        _id: Self::AssetId,
+        _who: &T::AccountId,
+        _amount: Self::Balance,
+    ) -> Result<Self::Balance, DispatchError> {
+        Ok(T::Balance::from(0u128))
+    }
+
+    fn burn_from(
+        _id: Self::AssetId,
+        _who: &T::AccountId,
+        _amount: Self::Balance,
+        _keep_alive: Preservation,
+        _precision: Precision,
+        _force: Fortitude,
+    ) -> Result<Self::Balance, DispatchError> {
+        Ok(T::Balance::from(0u128))
+    }
+}
+
+// MOCK: Implementation of fungibles::metadata::Inspect for Assets (required by metadata::Mutate)
 impl<T: Config> fungibles::metadata::Inspect<T::AccountId> for Pallet<T> {
     fn name(_id: Self::AssetId) -> Vec<u8> {
         b"Mock Asset".to_vec()
@@ -185,60 +185,6 @@ impl<T: Config> fungibles::metadata::Mutate<T::AccountId> for Pallet<T> {
         _symbol: Vec<u8>,
         _decimals: u8,
     ) -> DispatchResult {
-        Ok(()) // Always succeed
-    }
-}
-
-// MOCK: Implementation of fungibles::approvals::Inspect for Assets (might be required)
-impl<T: Config> fungibles::approvals::Inspect<T::AccountId> for Pallet<T> {
-    fn allowance(
-        _id: Self::AssetId,
-        _owner: &T::AccountId,
-        _delegate: &T::AccountId,
-    ) -> Self::Balance {
-        T::Balance::from(0u128)
-    }
-}
-
-// MOCK: Additional useful implementations that might be required by token_gateway
-impl<T: Config> fungibles::Create<T::AccountId> for Pallet<T> {
-    fn create(
-        _id: Self::AssetId,
-        _admin: T::AccountId,
-        _is_sufficient: bool,
-        _min_balance: Self::Balance,
-    ) -> DispatchResult {
         Ok(())
-    }
-}
-
-impl<T: Config> fungibles::Destroy<T::AccountId> for Pallet<T> {
-    fn start_destroy(
-        _id: Self::AssetId,
-        _maybe_check_owner: Option<T::AccountId>,
-    ) -> DispatchResult {
-        Ok(())
-    }
-
-    fn destroy_accounts(_id: Self::AssetId, _max_items: u32) -> Result<u32, DispatchError> {
-        Ok(0)
-    }
-
-    fn destroy_approvals(_id: Self::AssetId, _max_items: u32) -> Result<u32, DispatchError> {
-        Ok(0)
-    }
-
-    fn finish_destroy(_id: Self::AssetId) -> DispatchResult {
-        Ok(())
-    }
-}
-
-// Implementation of the AccountId conversion function using StaticLookup
-impl<T: Config> Pallet<T> {
-    pub fn lookup_account(
-        a: <T::Lookup as StaticLookup>::Source,
-    ) -> Result<T::AccountId, DispatchError> {
-        // Fixed the lookup error by mapping LookupError to DispatchError
-        T::Lookup::lookup(a).map_err(|_| DispatchError::Other("Lookup failed"))
     }
 }
