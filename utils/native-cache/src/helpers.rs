b8a0563633d1872f4a7de4809dffdc453570fe5a
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-04-19 17:26:57 +0200

    Update helpers.rs

--- utils/native-cache/src/helpers.rs
+++ utils/native-cache/src/helpers.rs
@@ -23,7 +23,7 @@ pub fn is_dyn_or_static_lib(p: impl AsRef<Path>) -> bool {
     }
 }
 
-/// check if the the file has the given name
+/// check if the file has the given name
 pub fn is_name(p: impl AsRef<Path>, name: &str) -> bool {
     p.as_ref().file_name().and_then(OsStr::to_str) == Some(name)
 }
