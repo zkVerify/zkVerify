6f6ae70ce99b6f3206fcaad335204b5fdd371bcd
Daniele Rogora <danielerogora@horizenlabs.io>
2025-06-06 16:50:37 +0200

    Update base, db, and all external pallets weights

--- runtime/src/weights/db.rs
+++ runtime/src/weights/db.rs
@@ -13,104 +13,101 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 36.0.0
-//! DATE: 2024-08-07 (Y/M/D)
-//! HOSTNAME: `bench1.fi`, CPU: `AMD Ryzen 7 7700 8-Core Processor`
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 46.2.0
+//! DATE: 2025-06-06 (Y/M/D)
+//! HOSTNAME: `6e834d009cf8`, CPU: `AMD Ryzen 7 7700 8-Core Processor`
 //!
-//! DATABASE: `RocksDb`, RUNTIME: `ZKV Testnet`
-//! BLOCK-NUM: `BlockId::Number(1213797)`
-//! SKIP-WRITE: `false`, SKIP-READ: `false`, WARMUPS: `1`
+//! DATABASE: `RocksDb`, RUNTIME: `zkVerify Volta`
+//! BLOCK-NUM: `BlockId::Number(0)`
+//! SKIP-WRITE: `false`, SKIP-READ: `false`, WARMUPS: `10`
 //! STATE-VERSION: `V1`, STATE-CACHE-SIZE: ``
-//! WEIGHT-PATH: `runtime/src/weights/db.rs`
+//! WEIGHT-PATH: `/data/benchmark/runtime/src/weights`
 //! METRIC: `Average`, WEIGHT-MUL: `1.1`, WEIGHT-ADD: `0`
 
 // Executed Command:
-//   ./target/release/zkv-node
+//   /usr/local/bin/zkv-relay
 //   benchmark
 //   storage
-//   --template-path
-//   node/zkv-db-weight-template.hbs
-//   --header
-//   HEADER-APACHE2
+//   --weight-path=/data/benchmark/runtime/src/weights
+//   --header=/data/benchmark/HEADER-APACHE2
+//   --warmups=10
 //   --mul
 //   1.1
 //   --state-version
 //   1
-//   --weight-path
-//   runtime/src/weights/db.rs
-//   -d
-//   /home/giacomo/zkv-node
+//   --base-path
+//   /data/benchmark/synced_root/
 
-/// Storage DB weights for the `ZKV Testnet` runtime and `RocksDb`.
+/// Storage DB weights for the `zkVerify Volta` runtime and `RocksDb`.
 pub mod constants {
-    use frame_support::weights::constants;
-    use sp_core::parameter_types;
-    use sp_weights::RuntimeDbWeight;
+	use frame_support::weights::constants;
+	use sp_core::parameter_types;
+	use sp_weights::RuntimeDbWeight;
 
-    parameter_types! {
-        /// By default, Substrate uses `RocksDB`, so this will be the weight used throughout
-        /// the runtime.
-        pub const RocksDbWeight: RuntimeDbWeight = RuntimeDbWeight {
-            // Time to read one storage item.
-            // Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
-            //
-            // Stats nanoseconds:
-            //   Min, Max: 972, 28_764
-            //   Average:  6_810
-            //   Median:   7_133
-            //   Std-Dev:  1328.79
-            //
-            // Percentiles nanoseconds:
-            //   99th: 9_467
-            //   95th: 8_526
-            //   75th: 7_474
-            read: 7_492 * constants::WEIGHT_REF_TIME_PER_NANOS,
+	parameter_types! {
+		/// By default, Substrate uses `RocksDB`, so this will be the weight used throughout
+		/// the runtime.
+		pub const RocksDbWeight: RuntimeDbWeight = RuntimeDbWeight {
+			// Time to read one storage item.
+			// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
+			//
+			// Stats nanoseconds:
+			//   Min, Max: 3_386, 149_864
+			//   Average:  8_520
+			//   Median:   8_556
+			//   Std-Dev:  9440.35
+			//
+			// Percentiles nanoseconds:
+			//   99th: 13_015
+			//   95th: 11_862
+			//   75th: 9_328
+			read: 9_372 * constants::WEIGHT_REF_TIME_PER_NANOS,
 
-            // Time to write one storage item.
-            // Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
-            //
-            // Stats nanoseconds:
-            //   Min, Max: 7_945, 1_994_029
-            //   Average:  20_322
-            //   Median:   15_449
-            //   Std-Dev:  19581.04
-            //
-            // Percentiles nanoseconds:
-            //   99th: 37_179
-            //   95th: 34_014
-            //   75th: 28_764
-            write: 22_355 * constants::WEIGHT_REF_TIME_PER_NANOS,
-        };
-    }
+			// Time to write one storage item.
+			// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
+			//
+			// Stats nanoseconds:
+			//   Min, Max: 13_716, 8_069_713
+			//   Average:  65_119
+			//   Median:   30_678
+			//   Std-Dev:  520015.57
+			//
+			// Percentiles nanoseconds:
+			//   99th: 68_921
+			//   95th: 40_456
+			//   75th: 36_379
+			write: 71_631 * constants::WEIGHT_REF_TIME_PER_NANOS,
+		};
+	}
 
-    #[cfg(test)]
-    mod test_db_weights {
-        use super::constants::RocksDbWeight as W;
-        use sp_weights::constants;
+	#[cfg(test)]
+	mod test_db_weights {
+		use super::constants::RocksDbWeight as W;
+		use sp_weights::constants;
 
-        /// Checks that all weights exist and have sane values.
-        // NOTE: If this test fails but you are sure that the generated values are fine,
-        // you can delete it.
-        #[test]
-        fn bound() {
-            // At least 1 µs.
-            assert!(
-                W::get().reads(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
-                "Read weight should be at least 1 µs."
-            );
-            assert!(
-                W::get().writes(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
-                "Write weight should be at least 1 µs."
-            );
-            // At most 1 ms.
-            assert!(
-                W::get().reads(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
-                "Read weight should be at most 1 ms."
-            );
-            assert!(
-                W::get().writes(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
-                "Write weight should be at most 1 ms."
-            );
-        }
-    }
+		/// Checks that all weights exist and have sane values.
+		// NOTE: If this test fails but you are sure that the generated values are fine,
+		// you can delete it.
+		#[test]
+		fn bound() {
+			// At least 1 µs.
+			assert!(
+				W::get().reads(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
+				"Read weight should be at least 1 µs."
+			);
+			assert!(
+				W::get().writes(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
+				"Write weight should be at least 1 µs."
+			);
+			// At most 1 ms.
+			assert!(
+				W::get().reads(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
+				"Read weight should be at most 1 ms."
+			);
+			assert!(
+				W::get().writes(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
+				"Write weight should be at most 1 ms."
+			);
+		}
+	}
 }
--- runtime/src/weights/extrinsic_weights.rs
+++ runtime/src/weights/extrinsic_weights.rs
@@ -13,66 +13,67 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 42.0.0
-//! DATE: 2025-02-26 (Y/M/D)
-//! HOSTNAME: `bench1.fi`, CPU: `AMD Ryzen 7 7700 8-Core Processor`
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 46.2.0
+//! DATE: 2025-06-05 (Y/M/D)
+//! HOSTNAME: `9f9a3dac6197`, CPU: `AMD Ryzen 7 7700 8-Core Processor`
 //!
 //! SHORT-NAME: `extrinsic`, LONG-NAME: `ExtrinsicBase`, RUNTIME: `Development`
 //! WARMUPS: `10`, REPEAT: `100`
-//! WEIGHT-PATH: `/home/danieler/zkVerify/runtime/src/weights`
+//! WEIGHT-PATH: `/data/benchmark/runtime/src/weights`
 //! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1.0`, WEIGHT-ADD: `0`
 
 // Executed Command:
-//   /home/danieler/zkVerify/target/production/zkv-relay
+//   /usr/local/bin/zkv-relay
 //   benchmark
 //   overhead
 //   --chain=dev
-//   --weight-path=/home/danieler/zkVerify/runtime/src/weights
-//   --header=/home/danieler/zkVerify/HEADER-APACHE2
+//   --weight-path=/data/benchmark/runtime/src/weights
+//   --header=/data/benchmark/HEADER-APACHE2
 //   --warmup=10
 //   --repeat=100
+//   --base-path=/tmp/tmp.LQE1fxMBsI
 
 use sp_core::parameter_types;
 use sp_weights::{constants::WEIGHT_REF_TIME_PER_NANOS, Weight};
 
 parameter_types! {
-    /// Time to execute a NO-OP extrinsic, for example `System::remark`.
-    /// Calculated by multiplying the *Average* with `1.0` and adding `0`.
-    ///
-    /// Stats nanoseconds:
-    ///   Min, Max: 53_995, 54_935
-    ///   Average:  54_489
-    ///   Median:   54_456
-    ///   Std-Dev:  209.14
-    ///
-    /// Percentiles nanoseconds:
-    ///   99th: 54_922
-    ///   95th: 54_817
-    ///   75th: 54_647
-    pub const ExtrinsicBaseWeight: Weight =
-        Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(54_489), 0);
+	/// Weight of executing a NO-OP extrinsic, for example `System::remark`.
+	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
+	///
+	/// Stats nanoseconds:
+	///   Min, Max: 103_928, 105_382
+	///   Average:  104_489
+	///   Median:   104_449
+	///   Std-Dev:  356.24
+	///
+	/// Percentiles nanoseconds:
+	///   99th: 105_330
+	///   95th: 105_178
+	///   75th: 104_688
+	pub const ExtrinsicBaseWeight: Weight =
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(104_489), 0);
 }
 
 #[cfg(test)]
 mod test_weights {
-    use sp_weights::constants;
+	use sp_weights::constants;
 
-    /// Checks that the weight exists and is sane.
-    // NOTE: If this test fails but you are sure that the generated values are fine,
-    // you can delete it.
-    #[test]
-    fn sane() {
-        let w = super::ExtrinsicBaseWeight::get();
+	/// Checks that the weight exists and is sane.
+	// NOTE: If this test fails but you are sure that the generated values are fine,
+	// you can delete it.
+	#[test]
+	fn sane() {
+		let w = super::ExtrinsicBaseWeight::get();
 
-        // At least 10 µs.
-        assert!(
-            w.ref_time() >= 10u64 * constants::WEIGHT_REF_TIME_PER_MICROS,
-            "Weight should be at least 10 µs."
-        );
-        // At most 1 ms.
-        assert!(
-            w.ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
-            "Weight should be at most 1 ms."
-        );
-    }
+		// At least 10 µs.
+		assert!(
+			w.ref_time() >= 10u64 * constants::WEIGHT_REF_TIME_PER_MICROS,
+			"Weight should be at least 10 µs."
+		);
+		// At most 1 ms.
+		assert!(
+			w.ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
+			"Weight should be at most 1 ms."
+		);
+	}
 }
