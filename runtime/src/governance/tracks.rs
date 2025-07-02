a51e99fc197c6b509a96d3618b204aad2845dd5d
DanieleDiBenedetto <daniele@horizenlabs.io>
2025-07-02 14:20:57 +0200

    Removed unneeded governance and treasury tracks + removed old migrations

--- runtime/src/governance/tracks.rs
+++ runtime/src/governance/tracks.rs
@@ -24,29 +24,12 @@ const fn percent(x: i32) -> sp_arithmetic::FixedI64 {
 use pallet_referenda::Curve;
 const APP_ROOT: Curve = Curve::make_reciprocal(4, 28, percent(80), percent(50), percent(100));
 const SUP_ROOT: Curve = Curve::make_linear(28, 28, percent(0), percent(50));
-const APP_STAKING_ADMIN: Curve = Curve::make_linear(17, 28, percent(50), percent(100));
-const SUP_STAKING_ADMIN: Curve =
-    Curve::make_reciprocal(12, 28, percent(1), percent(0), percent(50));
-const APP_TREASURER: Curve = Curve::make_reciprocal(4, 28, percent(80), percent(50), percent(100));
-const SUP_TREASURER: Curve = Curve::make_linear(28, 28, percent(0), percent(50));
 const APP_REFERENDUM_CANCELLER: Curve = Curve::make_linear(17, 28, percent(50), percent(100));
 const SUP_REFERENDUM_CANCELLER: Curve =
-    Curve::make_reciprocal(12, 28, percent(1), percent(0), percent(50));
-const APP_REFERENDUM_KILLER: Curve = Curve::make_linear(17, 28, percent(50), percent(100));
-const SUP_REFERENDUM_KILLER: Curve =
-    Curve::make_reciprocal(12, 28, percent(1), percent(0), percent(50));
-const APP_SMALL_TIPPER: Curve = Curve::make_linear(10, 28, percent(50), percent(100));
-const SUP_SMALL_TIPPER: Curve = Curve::make_reciprocal(1, 28, percent(4), percent(0), percent(50));
-const APP_BIG_TIPPER: Curve = Curve::make_linear(10, 28, percent(50), percent(100));
-const SUP_BIG_TIPPER: Curve = Curve::make_reciprocal(8, 28, percent(1), percent(0), percent(50));
-const APP_SMALL_SPENDER: Curve = Curve::make_linear(17, 28, percent(50), percent(100));
-const SUP_SMALL_SPENDER: Curve =
     Curve::make_reciprocal(12, 28, percent(1), percent(0), percent(50));
 const APP_MEDIUM_SPENDER: Curve = Curve::make_linear(23, 28, percent(50), percent(100));
 const SUP_MEDIUM_SPENDER: Curve =
     Curve::make_reciprocal(16, 28, percent(1), percent(0), percent(50));
-const APP_BIG_SPENDER: Curve = Curve::make_linear(28, 28, percent(50), percent(100));
-const SUP_BIG_SPENDER: Curve = Curve::make_reciprocal(20, 28, percent(1), percent(0), percent(50));
 
 const TRACKS_DATA: &[(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>)] = &[
     #[cfg(any(feature = "runtime-benchmarks", test))]
@@ -69,7 +52,7 @@ const TRACKS_DATA: &[(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>)] =
         pallet_referenda::TrackInfo {
             name: "wish_for_change",
             max_deciding: 10,
-            decision_deposit: 20 * THOUSANDS,
+            decision_deposit: THOUSANDS,
             prepare_period: 2 * HOURS,
             decision_period: 28 * DAYS,
             confirm_period: 24 * HOURS,
@@ -79,39 +62,11 @@ const TRACKS_DATA: &[(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>)] =
         },
     ),
     (
-        10,
-        pallet_referenda::TrackInfo {
-            name: "staking_admin",
-            max_deciding: 10,
-            decision_deposit: 5 * THOUSANDS,
-            prepare_period: 2 * HOURS,
-            decision_period: 28 * DAYS,
-            confirm_period: 3 * HOURS,
-            min_enactment_period: 10 * MINUTES,
-            min_approval: APP_STAKING_ADMIN,
-            min_support: SUP_STAKING_ADMIN,
-        },
-    ),
-    (
-        11,
-        pallet_referenda::TrackInfo {
-            name: "treasurer",
-            max_deciding: 10,
-            decision_deposit: THOUSANDS,
-            prepare_period: 2 * HOURS,
-            decision_period: 28 * DAYS,
-            confirm_period: 7 * DAYS,
-            min_enactment_period: 24 * HOURS,
-            min_approval: APP_TREASURER,
-            min_support: SUP_TREASURER,
-        },
-    ),
-    (
         20,
         pallet_referenda::TrackInfo {
             name: "referendum_canceller",
             max_deciding: 1_000,
-            decision_deposit: 10 * THOUSANDS,
+            decision_deposit: 5 * HUNDREDS,
             prepare_period: 2 * HOURS,
             decision_period: 7 * DAYS,
             confirm_period: 3 * HOURS,
@@ -121,87 +76,17 @@ const TRACKS_DATA: &[(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>)] =
         },
     ),
     (
-        21,
-        pallet_referenda::TrackInfo {
-            name: "referendum_killer",
-            max_deciding: 1_000,
-            decision_deposit: 50 * THOUSANDS,
-            prepare_period: 2 * HOURS,
-            decision_period: 28 * DAYS,
-            confirm_period: 3 * HOURS,
-            min_enactment_period: 10 * MINUTES,
-            min_approval: APP_REFERENDUM_KILLER,
-            min_support: SUP_REFERENDUM_KILLER,
-        },
-    ),
-    (
-        30,
-        pallet_referenda::TrackInfo {
-            name: "small_tipper",
-            max_deciding: 200,
-            decision_deposit: VFY,
-            prepare_period: MINUTES,
-            decision_period: 7 * DAYS,
-            confirm_period: 10 * MINUTES,
-            min_enactment_period: MINUTES,
-            min_approval: APP_SMALL_TIPPER,
-            min_support: SUP_SMALL_TIPPER,
-        },
-    ),
-    (
-        31,
-        pallet_referenda::TrackInfo {
-            name: "big_tipper",
-            max_deciding: 100,
-            decision_deposit: 10 * VFY,
-            prepare_period: 10 * MINUTES,
-            decision_period: 7 * DAYS,
-            confirm_period: HOURS,
-            min_enactment_period: 10 * MINUTES,
-            min_approval: APP_BIG_TIPPER,
-            min_support: SUP_BIG_TIPPER,
-        },
-    ),
-    (
-        32,
-        pallet_referenda::TrackInfo {
-            name: "small_spender",
-            max_deciding: 50,
-            decision_deposit: 100 * VFY,
-            prepare_period: 4 * HOURS,
-            decision_period: 28 * DAYS,
-            confirm_period: 2 * DAYS,
-            min_enactment_period: 24 * HOURS,
-            min_approval: APP_SMALL_SPENDER,
-            min_support: SUP_SMALL_SPENDER,
-        },
-    ),
-    (
         33,
         pallet_referenda::TrackInfo {
             name: "medium_spender",
             max_deciding: 50,
-            decision_deposit: 200 * VFY,
+            decision_deposit: THOUSANDS,
             prepare_period: 4 * HOURS,
             decision_period: 28 * DAYS,
             confirm_period: 4 * DAYS,
             min_enactment_period: 24 * HOURS,
             min_approval: APP_MEDIUM_SPENDER,
             min_support: SUP_MEDIUM_SPENDER,
-        },
-    ),
-    (
-        34,
-        pallet_referenda::TrackInfo {
-            name: "big_spender",
-            max_deciding: 50,
-            decision_deposit: 400 * VFY,
-            prepare_period: 4 * HOURS,
-            decision_period: 28 * DAYS,
-            confirm_period: 7 * DAYS,
-            min_enactment_period: 24 * HOURS,
-            min_approval: APP_BIG_SPENDER,
-            min_support: SUP_BIG_SPENDER,
         },
     ),
 ];
@@ -224,18 +109,9 @@ impl pallet_referenda::TracksInfo<Balance, BlockNumber> for TracksInfo {
         if let Ok(custom_origin) = origins::Origin::try_from(id.clone()) {
             match custom_origin {
                 origins::Origin::WishForChange => Ok(2),
-                // General admin
-                origins::Origin::StakingAdmin => Ok(10),
-                origins::Origin::Treasurer => Ok(11),
                 // Referendum admins
                 origins::Origin::ReferendumCanceller => Ok(20),
-                origins::Origin::ReferendumKiller => Ok(21),
-                // Limited treasury spenders
-                origins::Origin::SmallTipper => Ok(30),
-                origins::Origin::BigTipper => Ok(31),
-                origins::Origin::SmallSpender => Ok(32),
                 origins::Origin::MediumSpender => Ok(33),
-                origins::Origin::BigSpender => Ok(34),
             }
         } else {
             Err(())
