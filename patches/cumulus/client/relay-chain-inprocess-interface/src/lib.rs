473709d7835303642539dd613f71f5ae0d4e1051
la10736 <michele.damico@gmail.com>
2025-09-24 10:37:39 +0200

    Shut-up linter on patched crates
    Signed-off-by: la10736 <michele.damico@gmail.com>

--- patches/cumulus/client/relay-chain-inprocess-interface/src/lib.rs
+++ patches/cumulus/client/relay-chain-inprocess-interface/src/lib.rs
@@ -14,6 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
+// From rust 1.90.0 `RelayChainResult<(Arc<(dyn RelayChainInterface + 'static)>, Option<CollatorPair>)>`
+// become a lint warning due the `unused_parens`. That is the less invasive way to avoid it without
+// putting it in the middle of the code.
+#![allow(unused_parens)]
+
 use std::{
     collections::{BTreeMap, VecDeque},
     pin::Pin,
