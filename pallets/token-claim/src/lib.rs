3e8c497b4c3683be164ac8261dc2f03ace2897e7
DanieleDiBenedetto <daniele@horizenlabs.io>
2025-09-30 18:36:09 +0200

    fmt

--- pallets/token-claim/src/lib.rs
+++ pallets/token-claim/src/lib.rs
@@ -679,7 +679,7 @@ pub mod pallet {
 
         /// Claim tokens for a 'beneficiary' with an Ethereum address and send them to 'dest',
         /// provided a 'signature' on the actual claim message and 'dest'.
-        /// The message to sign should follow the EIP-191 standard. 
+        /// The message to sign should follow the EIP-191 standard.
         /// 'origin' must be none.
         /// Fails if:
         /// - 'beneficiary' is not entitled to any token
