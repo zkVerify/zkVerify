f80324028a550355cb3230625295bdc012d5addf
DimitrisPapac <dimpapach87@gmail.com>
2025-10-09 15:04:54 +0200

    More unit test for ezkl + fixes to old unit tests

--- verifiers/ezkl/src/verifier_should.rs
+++ verifiers/ezkl/src/verifier_should.rs
@@ -41,3 +41,118 @@ fn verify_valid_proof_alt() {
 
     assert!(Ezkl::<MockRuntime>::verify_proof(&vk, &proof, &pi).is_ok());
 }
+
+#[test]
+fn verify_vk_hash() {
+    let vk = EzklVk::new(VALID_VKA.to_vec());
+    let vk_hash = Ezkl::<MockRuntime>::vk_hash(&vk);
+
+    assert_eq!(
+        vk_hash.as_bytes(),
+        hex_literal::hex!("27833207b215f9d57bcf0adedaa5353bacd21e90252110f46b60943b54bd9518")
+    );
+}
+
+mod reject {
+    use super::*;
+
+    #[test]
+    fn invalid_public_values() {
+        let vk = EzklVk::new(VALID_VKA.to_vec());
+        let proof = VALID_PROOF.to_vec();
+
+        let mut invalid_pubs = valid_instances();
+        invalid_pubs[0][0] = 0x10;
+
+        assert_eq!(
+            Ezkl::<MockRuntime>::verify_proof(&vk, &proof, &invalid_pubs),
+            Err(VerifyError::VerifyError)
+        );
+    }
+
+    #[test]
+    fn if_provided_too_many_public_inputs() {
+        let vk = EzklVk::new(VALID_VKA.to_vec());
+        let proof = VALID_PROOF.to_vec();
+
+        let mut invalid_pubs = valid_instances();
+        while (invalid_pubs.len() as u32) <= <MockRuntime as Config>::MaxPubs::get() {
+            invalid_pubs.push(valid_instances()[0]);
+        }
+
+        assert_eq!(
+            Ezkl::<MockRuntime>::verify_proof(&vk, &proof, &invalid_pubs),
+            Err(VerifyError::InvalidInput)
+        );
+    }
+
+    #[test]
+    fn invalid_number_of_public_inputs() {
+        let vk = EzklVk::new(VALID_VKA.to_vec());
+        let proof = VALID_PROOF.to_vec();
+
+        let invalid_pubs = vec![valid_instances()[0]];
+
+        assert_eq!(
+            Ezkl::<MockRuntime>::verify_proof(&vk, &proof, &invalid_pubs),
+            Err(VerifyError::InvalidInput)
+        );
+    }
+
+    #[test]
+    fn big_proof() {
+        let vk = EzklVk::new(VALID_VKA.to_vec());
+        let pi = valid_instances();
+
+        let mut invalid_proof = VALID_PROOF.to_vec();
+        invalid_proof.push(1);
+
+        assert_eq!(
+            Ezkl::<MockRuntime>::verify_proof(&vk, &invalid_proof, &pi),
+            Err(VerifyError::InvalidProofData)
+        );
+    }
+
+    #[test]
+    fn small_proof() {
+        let vk = EzklVk::new(VALID_VKA.to_vec());
+        let pi = valid_instances();
+
+        let mut invalid_proof = VALID_PROOF.to_vec();
+        invalid_proof.pop();
+
+        assert_eq!(
+            Ezkl::<MockRuntime>::verify_proof(&vk, &invalid_proof, &pi),
+            Err(VerifyError::InvalidProofData)
+        );
+    }
+
+    #[test]
+    fn invalid_vk() {
+        let proof = VALID_PROOF.to_vec();
+        let pi = valid_instances();
+
+        let mut invalid_vk_bytes = VALID_VKA.to_vec();
+        invalid_vk_bytes[0] = 0x10;
+        let invalid_vk = EzklVk::new(invalid_vk_bytes);
+
+        assert_eq!(
+            Ezkl::<MockRuntime>::verify_proof(&invalid_vk, &proof, &pi),
+            Err(VerifyError::VerifyError) // note that this is not a `InvalidVerificationKey` since verification fails
+        );
+    }
+
+    #[test]
+    fn reject_malformed_proof() {
+        let vk = EzklVk::new(VALID_VKA.to_vec());
+        let pi = valid_instances();
+
+        let mut malformed_proof = VALID_PROOF.to_vec();
+        malformed_proof[0] = 0x07;
+
+        assert_eq!(
+            Ezkl::<MockRuntime>::verify_proof(&vk, &malformed_proof, &pi),
+            Err(VerifyError::InvalidProofData)
+        );
+    }
+}
