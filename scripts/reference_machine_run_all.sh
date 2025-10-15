7449553801984253fa65476779ce72e9da3450b4
DimitrisPapac <dimpapach87@gmail.com>
2025-10-15 14:39:36 +0200

    Added NETWORK to reference_machine_run_all.sh

--- scripts/reference_machine_run_all.sh
+++ scripts/reference_machine_run_all.sh
@@ -203,7 +203,7 @@ exlock_now || { echo -e "Error: this script cannot be run concurrently. Check no
 # this will trigger a docker build if needed
 # doing this before underclocking the CPU will be faster
 IS_BENCHMACHINE="false" ENABLE_PALLETS="false" ENABLE_OVERHEAD="false" ENABLE_MACHINE="false" \
-  sudo --preserve-env=IS_BENCHMACHINE,READ_IO,WRITE_IO,READ_BPS,WRITE_BPS,BENCH_BASE_PATH,DEVICE,USE_DOCKER,ENABLE_PALLETS,ENABLE_OVERHEAD,ENABLE_MACHINE,PALLETS,ECHO_CMD,ROOT_DIR \
+  sudo --preserve-env=IS_BENCHMACHINE,READ_IO,WRITE_IO,READ_BPS,WRITE_BPS,BENCH_BASE_PATH,DEVICE,USE_DOCKER,ENABLE_PALLETS,ENABLE_OVERHEAD,ENABLE_MACHINE,PALLETS,ECHO_CMD,ROOT_DIR,NETWORK \
   -u "${USER}" bash -c 'cd "${ROOT_DIR}"; "${ROOT_DIR}/scripts/run_all_benchmarks.sh"'
 
 # run benchmark
