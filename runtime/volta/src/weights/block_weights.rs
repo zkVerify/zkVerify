e1d9c9b2442f7ab67624ed593b1d2dce0db15de9
DimitrisPapac <dimpapach87@gmail.com>
2025-10-15 16:23:32 +0200

    Added remaining benchmarks for ezkl pallet

--- runtime/volta/src/weights/block_weights.rs
+++ runtime/volta/src/weights/block_weights.rs
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// 	http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,44 +14,44 @@
 // limitations under the License.
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 46.2.0
-//! DATE: 2025-06-05 (Y/M/D)
-//! HOSTNAME: `9f9a3dac6197`, CPU: `AMD Ryzen 7 7700 8-Core Processor`
+//! DATE: 2025-10-15 (Y/M/D)
+//! HOSTNAME: `6f65929b14f0`, CPU: `AMD Ryzen 7 7700 8-Core Processor`
 //!
 //! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Development`
 //! WARMUPS: `10`, REPEAT: `100`
-//! WEIGHT-PATH: `/data/benchmark/runtime/src/weights`
+//! WEIGHT-PATH: `/data/benchmark/runtime/volta/src/weights`
 //! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1.0`, WEIGHT-ADD: `0`
 
 // Executed Command:
 //   /usr/local/bin/zkv-relay
 //   benchmark
 //   overhead
-//   --chain=dev
-//   --weight-path=/data/benchmark/runtime/src/weights
+//   --chain=volta-dev
+//   --weight-path=/data/benchmark/runtime/volta/src/weights
 //   --header=/data/benchmark/HEADER-APACHE2
 //   --warmup=10
 //   --repeat=100
-//   --base-path=/tmp/tmp.LQE1fxMBsI
+//   --base-path=/tmp/tmp.aPl9slxYIh
 
 use sp_core::parameter_types;
 use sp_weights::{constants::WEIGHT_REF_TIME_PER_NANOS, Weight};
 
 parameter_types! {
-    /// Weight of executing an empty block.
-    /// Calculated by multiplying the *Average* with `1.0` and adding `0`.
-    ///
-    /// Stats nanoseconds:
-    ///   Min, Max: 425_994, 487_660
-    ///   Average:  434_558
-    ///   Median:   432_707
-    ///   Std-Dev:  6909.44
-    ///
-    /// Percentiles nanoseconds:
-    ///   99th: 447_795
-    ///   95th: 441_112
-    ///   75th: 437_316
-    pub const BlockExecutionWeight: Weight =
-        Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(434_558), 0);
+        /// Weight of executing an empty block.
+        /// Calculated by multiplying the *Average* with `1.0` and adding `0`.
+        ///
+        /// Stats nanoseconds:
+        ///   Min, Max: 422_830, 452_836
+        ///   Average:  431_175
+        ///   Median:   431_245
+        ///   Std-Dev:  4483.95
+        ///
+        /// Percentiles nanoseconds:
+        ///   99th: 438_770
+        ///   95th: 437_216
+        ///   75th: 434_322
+        pub const BlockExecutionWeight: Weight =
+                Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(431_175), 0);
 }
 
 #[cfg(test)]
