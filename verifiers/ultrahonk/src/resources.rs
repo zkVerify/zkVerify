b6eee1c16b63d5a4e35a68a17debed9f714af8b2
DimitrisPapac <dimpapach87@gmail.com>
2026-02-25 12:14:25 +0100

    Updated pallet to support v0.84 and v3.0+

--- verifiers/ultrahonk/src/resources.rs
+++ verifiers/ultrahonk/src/resources.rs
@@ -40,6 +40,15 @@ impl TestParams {
             protocol_version,
         }
     }
+
+    // For handling V0_84 where there is no log_circuit_size
+    pub fn new_v0_84(proof_type: ProofType) -> Self {
+        Self {
+            log_circuit_size: None,
+            proof_type,
+            protocol_version: ProtocolVersion::V0_84,
+        }
+    }
 }
 
 pub struct TestData {
@@ -65,7 +74,7 @@ pub fn get_parameterized_test_data(test_params: TestParams) -> Result<TestData, 
                 - MIN_BENCHMARKED_LOG_CIRCUIT_SIZE) as usize;
             let raw_test_data = &data[raw_test_data_idx];
 
-            let raw_vk: [u8; ultrahonk_no_std::VK_SIZE] = raw_test_data
+            let raw_vk: [u8; ultrahonk_no_std_v3_0::VK_SIZE] = raw_test_data
                 .vk
                 .try_into()
                 .expect("Benchmark file should always have the correct vk size");
@@ -84,8 +93,30 @@ pub fn get_parameterized_test_data(test_params: TestParams) -> Result<TestData, 
                 pubs,
             })
         }
-        _ => {
-            unreachable!()
+        ProtocolVersion::V0_84 => {
+            let raw_test_data = match test_params.proof_type {
+                ProofType::ZK => &DATA_V0_84_ZK,
+                ProofType::Plain => &DATA_V0_84_PLAIN,
+            };
+
+            let raw_vk: [u8; ultrahonk_no_std_v0_84::VK_SIZE] = raw_test_data
+                .vk
+                .try_into()
+                .expect("Benchmark file should always have the correct vk size");
+            let versioned_vk = VersionedVk::V0_84(raw_vk);
+            let proof = crate::Proof::new(test_params.proof_type, raw_test_data.proof.to_vec());
+            let versioned_proof = crate::VersionedProof::V0_84(proof);
+            let pubs = raw_test_data
+                .pubs
+                .chunks_exact(crate::PUB_SIZE)
+                .map(|c| c.try_into().unwrap())
+                .collect();
+
+            Ok(TestData {
+                versioned_vk,
+                versioned_proof,
+                pubs,
+            })
         }
     }
 }
@@ -96,198 +127,210 @@ struct Data {
     pubs: &'static [u8],
 }
 
+static DATA_V0_84_ZK: Data = Data {
+    vk: include_bytes!("resources/v0_84/zk/32/vk"),
+    proof: include_bytes!("resources/v0_84/zk/32/proof"),
+    pubs: include_bytes!("resources/v0_84/zk/32/pubs"),
+};
+
+static DATA_V0_84_PLAIN: Data = Data {
+    vk: include_bytes!("resources/v0_84/plain/32/vk"),
+    proof: include_bytes!("resources/v0_84/plain/32/proof"),
+    pubs: include_bytes!("resources/v0_84/plain/32/pubs"),
+};
+
 static DATA_PLAIN: &[Data] = &[
     Data {
-        vk: include_bytes!("resources/plain/log_7/vk"),
-        proof: include_bytes!("resources/plain/log_7/proof"),
-        pubs: include_bytes!("resources/plain/log_7/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_7/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_7/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_7/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_8/vk"),
-        proof: include_bytes!("resources/plain/log_8/proof"),
-        pubs: include_bytes!("resources/plain/log_8/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_8/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_8/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_8/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_9/vk"),
-        proof: include_bytes!("resources/plain/log_9/proof"),
-        pubs: include_bytes!("resources/plain/log_9/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_9/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_9/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_9/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_10/vk"),
-        proof: include_bytes!("resources/plain/log_10/proof"),
-        pubs: include_bytes!("resources/plain/log_10/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_10/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_10/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_10/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_11/vk"),
-        proof: include_bytes!("resources/plain/log_11/proof"),
-        pubs: include_bytes!("resources/plain/log_11/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_11/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_11/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_11/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_12/vk"),
-        proof: include_bytes!("resources/plain/log_12/proof"),
-        pubs: include_bytes!("resources/plain/log_12/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_12/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_12/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_12/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_13/vk"),
-        proof: include_bytes!("resources/plain/log_13/proof"),
-        pubs: include_bytes!("resources/plain/log_13/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_13/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_13/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_13/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_14/vk"),
-        proof: include_bytes!("resources/plain/log_14/proof"),
-        pubs: include_bytes!("resources/plain/log_14/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_14/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_14/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_14/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_15/vk"),
-        proof: include_bytes!("resources/plain/log_15/proof"),
-        pubs: include_bytes!("resources/plain/log_15/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_15/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_15/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_15/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_16/vk"),
-        proof: include_bytes!("resources/plain/log_16/proof"),
-        pubs: include_bytes!("resources/plain/log_16/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_16/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_16/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_16/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_17/vk"),
-        proof: include_bytes!("resources/plain/log_17/proof"),
-        pubs: include_bytes!("resources/plain/log_17/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_17/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_17/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_17/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_18/vk"),
-        proof: include_bytes!("resources/plain/log_18/proof"),
-        pubs: include_bytes!("resources/plain/log_18/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_18/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_18/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_18/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_19/vk"),
-        proof: include_bytes!("resources/plain/log_19/proof"),
-        pubs: include_bytes!("resources/plain/log_19/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_19/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_19/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_19/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_20/vk"),
-        proof: include_bytes!("resources/plain/log_20/proof"),
-        pubs: include_bytes!("resources/plain/log_20/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_20/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_20/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_20/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_21/vk"),
-        proof: include_bytes!("resources/plain/log_21/proof"),
-        pubs: include_bytes!("resources/plain/log_21/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_21/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_21/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_21/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_22/vk"),
-        proof: include_bytes!("resources/plain/log_22/proof"),
-        pubs: include_bytes!("resources/plain/log_22/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_22/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_22/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_22/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_23/vk"),
-        proof: include_bytes!("resources/plain/log_23/proof"),
-        pubs: include_bytes!("resources/plain/log_23/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_23/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_23/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_23/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_24/vk"),
-        proof: include_bytes!("resources/plain/log_24/proof"),
-        pubs: include_bytes!("resources/plain/log_24/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_24/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_24/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_24/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/plain/log_25/vk"),
-        proof: include_bytes!("resources/plain/log_25/proof"),
-        pubs: include_bytes!("resources/plain/log_25/pubs"),
+        vk: include_bytes!("resources/v3_0/plain/log_25/vk"),
+        proof: include_bytes!("resources/v3_0/plain/log_25/proof"),
+        pubs: include_bytes!("resources/v3_0/plain/log_25/pubs"),
     },
 ];
 
 static DATA_ZK: &[Data] = &[
     Data {
-        vk: include_bytes!("resources/zk/log_7/vk"),
-        proof: include_bytes!("resources/zk/log_7/proof"),
-        pubs: include_bytes!("resources/zk/log_7/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_7/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_7/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_7/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_8/vk"),
-        proof: include_bytes!("resources/zk/log_8/proof"),
-        pubs: include_bytes!("resources/zk/log_8/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_8/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_8/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_8/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_9/vk"),
-        proof: include_bytes!("resources/zk/log_9/proof"),
-        pubs: include_bytes!("resources/zk/log_9/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_9/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_9/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_9/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_10/vk"),
-        proof: include_bytes!("resources/zk/log_10/proof"),
-        pubs: include_bytes!("resources/zk/log_10/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_10/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_10/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_10/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_11/vk"),
-        proof: include_bytes!("resources/zk/log_11/proof"),
-        pubs: include_bytes!("resources/zk/log_11/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_11/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_11/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_11/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_12/vk"),
-        proof: include_bytes!("resources/zk/log_12/proof"),
-        pubs: include_bytes!("resources/zk/log_12/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_12/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_12/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_12/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_13/vk"),
-        proof: include_bytes!("resources/zk/log_13/proof"),
-        pubs: include_bytes!("resources/zk/log_13/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_13/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_13/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_13/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_14/vk"),
-        proof: include_bytes!("resources/zk/log_14/proof"),
-        pubs: include_bytes!("resources/zk/log_14/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_14/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_14/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_14/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_15/vk"),
-        proof: include_bytes!("resources/zk/log_15/proof"),
-        pubs: include_bytes!("resources/zk/log_15/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_15/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_15/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_15/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_16/vk"),
-        proof: include_bytes!("resources/zk/log_16/proof"),
-        pubs: include_bytes!("resources/zk/log_16/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_16/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_16/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_16/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_17/vk"),
-        proof: include_bytes!("resources/zk/log_17/proof"),
-        pubs: include_bytes!("resources/zk/log_17/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_17/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_17/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_17/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_18/vk"),
-        proof: include_bytes!("resources/zk/log_18/proof"),
-        pubs: include_bytes!("resources/zk/log_18/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_18/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_18/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_18/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_19/vk"),
-        proof: include_bytes!("resources/zk/log_19/proof"),
-        pubs: include_bytes!("resources/zk/log_19/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_19/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_19/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_19/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_20/vk"),
-        proof: include_bytes!("resources/zk/log_20/proof"),
-        pubs: include_bytes!("resources/zk/log_20/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_20/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_20/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_20/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_21/vk"),
-        proof: include_bytes!("resources/zk/log_21/proof"),
-        pubs: include_bytes!("resources/zk/log_21/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_21/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_21/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_21/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_22/vk"),
-        proof: include_bytes!("resources/zk/log_22/proof"),
-        pubs: include_bytes!("resources/zk/log_22/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_22/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_22/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_22/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_23/vk"),
-        proof: include_bytes!("resources/zk/log_23/proof"),
-        pubs: include_bytes!("resources/zk/log_23/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_23/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_23/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_23/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_24/vk"),
-        proof: include_bytes!("resources/zk/log_24/proof"),
-        pubs: include_bytes!("resources/zk/log_24/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_24/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_24/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_24/pubs"),
     },
     Data {
-        vk: include_bytes!("resources/zk/log_25/vk"),
-        proof: include_bytes!("resources/zk/log_25/proof"),
-        pubs: include_bytes!("resources/zk/log_25/pubs"),
+        vk: include_bytes!("resources/v3_0/zk/log_25/vk"),
+        proof: include_bytes!("resources/v3_0/zk/log_25/proof"),
+        pubs: include_bytes!("resources/v3_0/zk/log_25/pubs"),
     },
 ];
